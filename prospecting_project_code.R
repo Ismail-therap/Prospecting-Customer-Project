a9d9e49972a770de4fb14d49ec318802ea00c5c7
Ismail-therap <statistician71@gmail.com>
2019-05-25 21:39:03 +0600

    updating code

--- prospecting_project_code.R
+++ prospecting_project_code.R
@@ -5,9 +5,8 @@
 
 library(readr)
 
-dat <- read_csv("C:/Users/Ayota/Desktop/Data/Prospecting Modelv1.csv")
+dat <- read_csv("C:/Users/Ayota/Desktop/Data/Prospecting Modelv1.csv",na = "NULL")
 
-str(dat)
 
 dat_2017 <- read_csv("C:/Users/Ayota/Desktop/Data/Historical_TF_File_Qith_NBN_All_Presale_2017_Update.csv")
 dat_2018 <- read_csv("C:/Users/Ayota/Desktop/Data/Historical_TF_File_Qith_NBN_All_Presale_2018_Update.csv")
@@ -31,39 +30,67 @@ colnames(comb_17_and_18)[1] <- "ID"
 comb_dat <- merge(x = dat, y = comb_17_and_18, by = "ID", all.x = TRUE)
 
 
-### Recoding the dependent variable:
 
-
-str(comb_dat)
 #########################################
 #### 
 
 
 
 
+# Function to calculate the most frequent object in a vector:
+getMode <- function(myvector) {
+  mytable <- table(myvector)
+  return(names(mytable)[which.max(mytable)])
+}
 
 
-#############################Fitting Logistic Regression####################################
+#### Subsetting the data file with our selected columns:
+
+comb_data <- comb_dat[,c("ID","primary_medical_funding__c","segment_sub","salesoffice","market",
+                         "minimum_value","avg_weighted_tf")]
+
+colnames(comb_data)[6] <- "minimum_of_nbn"
+
+# Missing value percentage by variable:
+missing_value_per <- colMeans(is.na(comb_data))*100
+missing_value_per
+
+### Exploring the distribution of avg_weighted_tf and the minimum of nbn variables:
+
+library(ggplot2)
+require(reshape2)
+df <- comb_data[,c("minimum_of_nbn","avg_weighted_tf")]
+ggplot(melt(df), aes(value, fill = variable)) + geom_histogram(position = "dodge")
 
 
-myData <-read.csv("logistic_regression.csv")
-head(myData)
-attach(myData)
+### Replacing missing values:
 
-GENDERCODE<-as.factor(GENDERCODE)
-LOCALITY<-as.factor(LOCALITY)
-STATECODE<-as.factor(STATECODE)
-income_band_dsc<-as.factor(income_band_dsc)
-
-age_band_dsc<-as.factor(age_band_dsc)
-PD_DSC<-as.factor(PD_DSC)
-Churn<-as.factor(Churn)
-
-summary(myData)
+library(data.table)
+comb_data <- data.table(comb_data)
 
 
+comb_data[is.na(primary_medical_funding__c), primary_medical_funding__c:= getMode(comb_data[,primary_medical_funding__c])]
+comb_data[is.na(segment_sub), segment_sub:= getMode(comb_data[,segment_sub])]
+comb_data[is.na(salesoffice), salesoffice:= getMode(comb_data[,salesoffice])]
+comb_data[is.na(market), market:= getMode(comb_data[,market])]
 
-my.glm<-glm(Churn~LOCALITY, family=binomial(link = "logit"))
+comb_data[is.na(minimum_of_nbn), minimum_of_nbn:= mean(comb_data[,minimum_of_nbn],na.rm = T)]
+comb_data[is.na(avg_weighted_tf), avg_weighted_tf:= mean(comb_data[,avg_weighted_tf],na.rm = T)]
+
+# making sure there is no missing values:
+colMeans(is.na(comb_data))*100 # if every entry is 0 then we are done!
+
+
+#####################################
+######Fitting Logistic Regression ###
+#####################################
+
+# Recategorizing the dependent variable:
+str(comb_data)
+
+
+
+my.glm <-glm(Churn~LOCALITY, family=binomial(link = "logit"))
 
 summary(my.glm)
 names(my.glm)
